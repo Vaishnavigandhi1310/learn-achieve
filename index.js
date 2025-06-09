bb04de281b14e49a69a484db1102b822f5621284
Vaishnavi Gandhi <vaishnavii8319@gmail.com>
2025-06-09 16:48:19 +0530

    done

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-// const express = require('express');
-// const dotenv = require('dotenv');
-// const path = require('path');         
-// const cors = require('cors');
-// const connectDB = require('./config/db');
-// const authRoutes = require('./routes/authRoutess');
-// const packageRoutes = require('./routes/packageRoutes');
-// const cartRoutes = require('./routes/cartRoutes');
-// const orderRoutes = require('./routes/orderRoutes');
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');         
+const cors = require('cors');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutess');
+const packageRoutes = require('./routes/packageRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
-// dotenv.config();
+dotenv.config();
 
-// const app = express();
-// connectDB();
+const app = express();
+connectDB();
 
-// app.use(cors());
+app.use(cors());
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// app.use('/style/upload', express.static(path.join(__dirname, 'upload')));  
+app.use('/style/upload', express.static(path.join(__dirname, 'upload')));  
 
-// app.use('/api/packages', packageRoutes);
-// app.use('/api/auth', authRoutes);
-// app.use('/api/cart', cartRoutes);
-// app.use('/api/orders', orderRoutes);
+app.use('/api/packages', packageRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/orders', orderRoutes);
 
-// app.get('/', (req, res) => {
-//   res.send('API is running...');
-// });
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
-// const PORT = process.env.PORT || 8080;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
