29bad840af411fe50a7b27db99aa75b0f62a1c05
Vaishnavi Gandhi <vaishnavii8319@gmail.com>
2025-06-09 10:45:01 +0530

    order

--- models/Order.js
+++ models/Order.js
@@ -1,3 +1,43 @@
+// const mongoose = require('mongoose');
+
+// const orderSchema = new mongoose.Schema({
+//   userId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     required: true,
+//     // ref: 'User',
+//   },
+//   items: [
+//     {
+//       packageId: { type: mongoose.Schema.Types.ObjectId,
+//          ref: 'Package', 
+//          required: true
+//          },
+//       quantity:
+//        { type: Number,
+//          default: 1 
+//         },
+//       amount:
+//        { type: Number, 
+//         required: true 
+//     },
+//       discountAmount: {
+//          type: Number
+//          },
+//     }
+//   ],
+//   totalAmount: { 
+//     type: Number, 
+//     required: true 
+// },
+//   status: {
+//      type: String,
+//      default: 'Pending' 
+//     },
+// }, {
+//      timestamps: true
+//      });
+
+// module.exports = mongoose.model('Order', orderSchema);
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
